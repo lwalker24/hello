d9195f8a01c23d945d6dd1857722b96bed8c4251
Your Name <youremail@huskers.unl.edu>
2024-08-29 10:21:38 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Henry Monahan
+ * Date: 08/29/24
+ *
+ * A simple Henry Monahan printer program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+
+  printf("Henry Monahan");
+
+  return 0;
+}
