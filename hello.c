b64f882ef6295a9031c74de37b4a10ae11cb8a0c
lwalker24 <lwalker24@huskers.unl.edu>
2024-09-03 11:24:38 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Luke Walker
+ *         lwalker24@huskers.unl.edu
+ * Date: 2024/08/27
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Luke Walker\n");
+  return 0;
+}
+
+
